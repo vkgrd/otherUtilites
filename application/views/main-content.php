fd1d58261c82bdf4725a659726141e6ec646ba6f
Denis Hardenberg <aaronbe@ya.ru>
2016-08-22 17:59:58 +0500

    First catalog elements example

---
+++ application/views/main-content.php
@@ -0,0 +1,24 @@
+<div class="catalog">
+	<div class="container">
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+		<div class="element"><i class="fa fa-plus"></i></div>
+	</div>
+</div>
